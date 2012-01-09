25cb1409440b9974aa8d61dc1b091ca3802abf8c
Alexandra Kuznetsova <alku@dtu.dk>
2012-01-09 00:00:00 +0000

    version 2.0-0

--- R/classes.R
+++ R/classes.R
@@ -1,2 +1,5 @@
-merLmerTest<-setClass("merLmerTest", representation(t.pval="numeric") ,contains = "mer")
-summary.merLmerTest<-setClass("summary.merLmerTest", contains = "summary.mer")
+merModLmerTest <- setClass("merModLmerTest", contains = c("merMod", "lmerMod"))
+
+  
+
+
